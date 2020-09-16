50293bbcda45d6d51eceb9030b8486742b1c0578
yunwei37 <1067852565@qq.com>
2020-09-16 07:09:11 -0700

    final

---
+++ README.md
@@ -0,0 +1,14 @@
+# Simple Ray Tracer
+
+This is a simple ray tracer from [Ray Tracing in One Weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html) for learning purpose, with some improvements
+
+## usage
+
+- generate ppm image:
+    ```
+    make img
+    ```
+- clean up
+    ```
+    make clean
+    ```
--- material.h
+++ material.h
@@ -31,19 +31,49 @@ class lambertian : public material {
 
 class metal : public material {
     public:
-        metal(const color& a) : albedo(a) {}
+        metal(const color& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
 
         virtual bool scatter(
             const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
         ) const override {
             vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-            scattered = ray(rec.p, reflected);
+            scattered = ray(rec.p, reflected + fuzz*random_unit_vector());
             attenuation = albedo;
             return (dot(scattered.direction(), rec.normal) > 0);
         }
 
     public:
         color albedo;
+        double fuzz;
+};
+
+class dielectric : public material {
+    public:
+        dielectric(double ri) : ref_idx(ri) {}
+
+        virtual bool scatter(
+            const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+        ) const override {
+            attenuation = color(1.0, 1.0, 1.0);
+            double etai_over_etat = rec.front_face ? (1.0 / ref_idx) : ref_idx;
+
+            vec3 unit_direction = unit_vector(r_in.direction());
+
+            double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
+            double sin_theta = sqrt(1.0 - cos_theta*cos_theta);
+            if (etai_over_etat * sin_theta > 1.0 ) {
+                vec3 reflected = reflect(unit_direction, rec.normal);
+                scattered = ray(rec.p, reflected);
+                return true;
+            }
+
+            vec3 refracted = refract(unit_direction, rec.normal, etai_over_etat);
+            scattered = ray(rec.p, refracted);
+            return true;
+        }
+
+    public:
+        double ref_idx;
 };
 
 #endif
